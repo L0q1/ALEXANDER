1f2ca16666a5560da0e7b8c6510ce8b020d52b8c
L0q1 <93480756+L0q1@users.noreply.github.com>
2025-06-23 19:11:17 +0200

    Refactor code
    v2.0.0
    
    Remove most of the comments
    The code is pretty readable and self-explanatory.
    
    Add flags
    Change shebang to more portable one
    Improve check for already running instance of the script

--- alexander.sh
+++ alexander.sh
@@ -1,176 +1,209 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-##################################################################################################################
+##################################################################################################
 # ALEXANDER - A Linux Equivalent of Source Auto-Named Demo Recorder
-# Bash script that writes "record <timestamp>" to set .cfg file every X seconds when the game is running
-# Config can then be executed during the game to start recording a demo with unique timestamp
+# Script that writes "record <timestamp>" to CFG file every X seconds when the game is running
+# Config can then be executed during the game to start recording timestamped demo
 #
-# Inspired by Source Auto-Named Demo Recorder (SANDER) by Sander Dijkstra (https://www.dyxtra.com/sander)
-##################################################################################################################
+# Inspired by Sander Dijkstra's "SANDER" (https://www.dyxtra.com/sander)
+##################################################################################################
 
-# Settings
-cfg_folder="$HOME/.steam/steam/steamapps/common/Left 4 Dead 2/left4dead2/cfg" # string # path to cfg folder
-cfg_name='sander.cfg' # string # name of the file to write the command to (whole file will be overwritten!)
-demo_prefix='autorec_' # string # example of a demo name with default prefix: autorec_2023-06-04-12-53-04.dem
-process_name='hl2_linux' # string # start refreshing the timestamp when this process is found running
-check_for_games='30' # integer # check for the process specified above every X seconds
-refresh_command='10' # integer # refresh the timestamp every X seconds if process is running
+settings() {
+  # string # Path to cfg folder
+  cfg_folder="$HOME/.steam/steam/steamapps/common/Left 4 Dead 2/left4dead2/cfg"
 
-# Shell colors
-cred="\033[31m"
-cgreen="\033[32m"
-cyellow="\033[33m"
-cblue="\033[34m"
-cstop="\033[0m"
+  # string # Name of the file to write the command to (must be unique)
+  cfg_file='sander.cfg'
 
-# FUNCTION: bash messages
-bash_error() { echo -e "${cred}$1${cstop} $2"; }
-bash_warn() { echo -e "${cyellow}$1${cstop} $2"; }
-bash_success() { echo -e "${cgreen}$1${cstop} $2"; }
-bash_info() { echo -e "${cblue}$1${cstop} $2 ${cblue}$3${cstop} $4"; }
+  # string # Demo name with default prefix: autorec_2023-06-04-12-53-04.dem
+  demo_prefix='autorec_'
 
-# FUNCTION: send libnotify notification to desktop if possible
+  # string # Start refreshing the timestamp when this process is found
+  process_name='hl2_linux'
+
+  # integer # Check for the process every X seconds
+  check_for_games='30'
+
+  # integer # Refresh the timestamp every X seconds
+  refresh_command='10'
+}
+
+help_message() {
+  echo "Usage: ${0##*/} [options]"
+  echo "Source engine demo recording helper."
+  echo
+  echo "Options:"
+  echo "  -e, --edit       Edit the script and exit."
+  echo "  -h, --help       Display help and exit."
+  echo "  -v, --version    Show version and exit."
+  echo
+  echo "Customizations can be applied inside the script in the 'settings' function."
+}
+
+bash_error() { echo -e "${cred}$1${cstop} ${@:2}"; }
+bash_warn() { echo -e "${cyellow}$1${cstop}" ${@:2}; }
+bash_success() { echo -e "${cgreen}$1${cstop}" ${@:2}; }
+bash_info() { echo -e "${cblue}$1${cstop} $2 ${cblue}$3${cstop} ${@:4}"; }
+
 desktop_notification() {
   if command -v notify-send > /dev/null 2>&1; then
-    notify-send -a ALEXANDER "$1" "$2"
+    notify-send -a ALEXANDER "$@"
   fi
 }
 
-# CHECK: is ALEXANDER already running
-if pidof -x "$(basename -- $0)" -o $$ > /dev/null 2>&1; then
-  desktop_notification "ALEXANDER is already running!" "Process ID: $(pidof -x "$(basename -- $0)" -o $$)"
-  bash_warn "This script is already running. PID:" "$(pidof -x "$(basename -- $0)" -o $$)"
-  exit 114
-fi
-
-# FUNCTION: is integer and 0<
-is_valid_int() {
-  case $1 in
-    ''|*[!0-9]*|0)
-      desktop_notification "ERROR" "Invalid value '$1' in '$2' setting.\nInsert a number (in seconds) instead."
-      bash_error "ERROR: '$2' is assigned an invalid value:" "$1"
-      exit 22 ;;
-    *) ;;
-  esac
-}
-
-# CHECK: are variables present and valid
-is_valid_int $check_for_games check_for_games
-is_valid_int $refresh_command refresh_command
-
-# Remove last '/' in directory path if present
-cfg_folder=${cfg_folder%/}
-
-# Append '.cfg' extension to config name if absent
-if [[ $cfg_name != *.cfg ]]; then
-  cfg_name=$cfg_name.cfg
-fi
-
-# Set up full path to the config
-cfg_path=$cfg_folder/$cfg_name
-
-# CHECK: does config directory exist
-if [[ ! -d $cfg_folder ]]; then
-  desktop_notification "ERROR" "Config directory not found."
-  bash_error "ERROR: Directory not found:" "$cfg_folder"
-  exit 2
-fi
-
-# CHECK: set config file / create if doesn't exist
-if [[ -s $cfg_path ]]; then
-  desktop_notification "ERROR" "'$cfg_name' exists but is not empty."
-  bash_error "ERROR: Config exists but is not empty:" "$cfg_path"
-  exit 27
-elif [[ -w $cfg_path ]]; then
-  bash_success "Config set:" "$cfg_path"
-elif [[ ! -f $cfg_path ]]; then
-  bash_warn "Config file '$cfg_name' not found. Creating..."
-  if [[ -w $cfg_folder ]]; then
-    :>"$cfg_path"
-    bash_success "Done!"
-    bash_success "Config set:" "$cfg_path"
-  else
-    desktop_notification "ERROR" "Cannot create '$cfg_name'."
-    bash_error "ERROR: Directory is read-only:" "$cfg_folder"
-    exit 13
-  fi
-else
-  desktop_notification "ERROR" "Cannot write to '$cfg_name'."
-  bash_error "ERROR: Config is read-only:" "$cfg_path"
-  exit 13
-fi
-
-# FUNCTION: update timestamp and write command to config
 write_command() {
   timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
-  cfg_command="record $demo_prefix$timestamp"
-  echo $cfg_command > "$cfg_path"
+  cfg_command="record ${demo_prefix}${timestamp}"
+  echo "$cfg_command" > "$cfg_path"
 }
 
-# FUNCTION: cleanup on dirty exit
-dirty_exit() {
+cleanup_dirty_exit() {
   desktop_notification "Warning!" "Script terminated."
   bash_warn "\nScript terminated."
-  :>"$cfg_path"
+  rm "$cfg_path"
   exit 130
 }
 
-# Perform cleanup on SIGINT (Ctrl+C) and SIGTERM ('End Process...')
-trap "dirty_exit" 2 15
+main() {
 
-# Start scanning & user input
-work_var=0
-bash_success "Looking for the process:" "$process_name"
-while [[ -w $cfg_path ]]; do
-  if pgrep -x $process_name > /dev/null; then
-    if [[ $work_var == 0 ]]; then
-      bash_success "Process found!"
-      work_var=1
+  version=2.0.0
+
+  cred="\033[31m"
+  cgreen="\033[32m"
+  cyellow="\033[33m"
+  cblue="\033[34m"
+  cstop="\033[0m"
+
+  settings
+
+  cfg_folder="${cfg_folder%/}"
+  [[ $cfg_file != *.cfg ]] && cfg_file="${cfg_file}.cfg"
+  cfg_path="${cfg_folder}/${cfg_file}"
+
+  # Parse all positional parameters
+  while [[ "$1" != "" ]]; do
+    case "$1" in
+
+      '-e'|'--edit')
+        [[ ! $EDITOR ]] && echo '$EDITOR variable not found!' && exit 127
+        echo "Editing \"$0\""
+        "$EDITOR" "$0"
+        exit 0
+        ;;
+
+      '-h'|'--help')
+        help_message
+        exit 0
+        ;;
+
+      '-v'|'--version')
+        echo "       _                          _           "
+        echo "  __ _| | _____  ____ _ _ __   __| | ___ _ __ "
+        echo " / _' | |/ _ \ \/ / _' | '_ \ / _' |/ _ \ '__|"
+        echo "| (_| | |  __/>  < (_| | | | | (_| |  __/ |   "
+        echo " \__,_|_|\___/_/\_\__,_|_| |_|\__,_|\___|_|.sh"
+        echo
+        echo "A Linux Equivalent of Source Auto-Named Demo Recorder v${version}"
+        echo "Copyright (c) 2023 L0q1 - MIT license"
+        echo
+        exit 0
+        ;;
+
+      *)
+        echo "Invalid option: \"$1\""
+        help_message
+        exit 1
+        ;;
+
+    esac
+    shift
+  done
+
+  for int in $check_for_games $refresh_command; do
+    if [[ ! $int =~ ^[0-9]+$ ]] || [[ $int -le 0 ]]; then
+      desktop_notification "ERROR" "Invalid integer in settings."
+      bash_error "ERROR: Invalid integer in settings" \"$int\"
+      exit 22
     fi
-    write_command
-    read -r -t $refresh_command user_input
-  else
-    if [[ $work_var == 1 ]]; then
-      bash_warn "Process lost."
-      :>"$cfg_path"
-      work_var=0
-    fi
-    read -r -t $check_for_games user_input
+  done
+
+  if [[ ! -d $cfg_folder ]]; then
+    desktop_notification "ERROR" "Directory not found."
+    bash_error "ERROR: Directory not found" \"$cfg_folder\"
+    exit 2
+  elif [[ ! -w $cfg_folder ]]; then
+    desktop_notification "ERROR" "Cannot access directory."
+    bash_error "ERROR: Directory is read-only" \"$cfg_folder\"
+    exit 13
   fi
 
-  case $user_input in
-    help)
-      bash_info "help             " "print all available commands"
-      bash_info "status           " "check script status"
-      bash_info "quit, stop, exit " "stop the script" ;;
-    status)
-      if [[ $work_var == 0 ]]; then
-        bash_info "Looking for" "$process_name" "every" "$check_for_games"s
-      else
-        bash_info "Found" "$process_name"
-        bash_info "Updating timestamp in" "$cfg_name" "every" "$refresh_command"s
-      fi ;;
-    quit|stop|exit)
-      bash_success "Script stopped."
-      :>"$cfg_path"
-      exit 0 ;;
-    '') ;;
-    *) bash_info "Type" "help" "for a list of available commands." ;;
-  esac
-done
+  if [[ -f $cfg_path ]]; then
+    desktop_notification "ERROR" "Not allowed to overwrite \"$cfg_file\"."
+    bash_error "ERROR: File exists" \"$cfg_path\"
+    exit 17
+  else
+    :>"$cfg_path"
+    bash_success "Config set:" \"$cfg_path\"
+  fi
 
-# Other exit reasons
-if [[ ! -f $cfg_path ]]; then
-  desktop_notification "Warning!" "Config deleted."
-  bash_warn "Config deleted."
-  exit 2
-elif [[ ! -w $cfg_path ]]; then
-  desktop_notification "Warning!" "Config permissions changed."
-  bash_warn "Config permissions changed."
-  exit 13
-else
-  desktop_notification "Exiting!" "Reason unknown."
-  bash_warn "Exiting. Reason unknown."
-  :>"$cfg_path"
-  exit 1
-fi
+  trap "cleanup_dirty_exit" SIGINT SIGTERM
+
+  # Run the program
+  bash_success "Looking for:" \"$process_name\"
+
+  while [[ -w $cfg_path ]]; do
+    if pgrep -x $process_name > /dev/null 2>&1; then
+      if [[ ! -s $cfg_path ]]; then
+        bash_success "Process \"$process_name\" found!"
+      fi
+      write_command
+      read -r -t $refresh_command user_input
+    else
+      if [[ -s $cfg_path ]]; then
+        bash_warn "Process lost."
+        :>"$cfg_path"
+      fi
+      read -r -t $check_for_games user_input
+    fi
+
+    case $user_input in
+      's'|'status')
+        if [[ ! -s $cfg_path ]]; then
+          bash_info "Looking for" "\"$process_name\"" "every" "${check_for_games}s"
+        else
+          bash_info "Updating timestamp in" "\"$cfg_file\"" "every" "${refresh_command}s"
+        fi
+        ;;
+      'q'|'quit'|'stop'|'exit')
+        bash_success "Script stopped."
+        rm "$cfg_path"
+        exit 0
+        ;;
+      '')
+        ;;
+      *)
+        bash_info "s, status           " "Check script status."
+        bash_info "q, quit, stop, exit " "Stop the script."
+        ;;
+    esac
+  done
+
+  if [[ ! -f $cfg_path ]]; then
+    desktop_notification "Unexpected exit!" "Config deleted."
+    bash_warn "Exiting. Config deleted."
+    exit 2
+  elif [[ ! -w $cfg_path ]]; then
+    desktop_notification "Unexpected exit!" "Config permissions changed."
+    bash_warn "Exiting. Config permissions changed."
+    rm -f "$cfg_path"
+    exit 13
+  else
+    desktop_notification "Unexpected exit!" "Reason unknown."
+    bash_warn "Exiting. Reason unknown."
+    rm "$cfg_path"
+    exit 1
+  fi
+
+}
+
+main "$@"
